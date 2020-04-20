682037c1cc9c3cb88b1ec66789f1b3fc62ae3638
Lilith <lilith.hovhann@gmail.com>
2020-04-20 19:31:36 +0400

    generate some data

--- patientcare/src/main/java/io/project/app/patientcare/PatientcareApplication.java
+++ patientcare/src/main/java/io/project/app/patientcare/PatientcareApplication.java
@@ -1,9 +1,15 @@
 package io.project.app.patientcare;
 
+import io.project.app.patientcare.models.Patient;
+import io.project.app.patientcare.services.PatientService;
+import java.util.Date;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.event.EventListener;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
@@ -15,5 +21,41 @@ public class PatientcareApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PatientcareApplication.class, args);
 	}
+        
+        @Autowired
+        private PatientService patientService;
+        
+        @EventListener(ApplicationReadyEvent.class) //this works after start of app
+        public void populateData(){
+            Patient patient = new Patient();
+            patient.setActive(true);
+            patient.setBirthdate(new Date());
+            patient.setFirstname("Emilia");
+            patient.setLastname("Clarke");
+            patient.setGender("female");
+            patient.setHealthStatus("too bad");
+            
+            patient.setMartialStatus("married");
+            patient.setPatientRegisterDate(new Date());
+            patient.setPhone("010288888");
+            patient.setStatus(1);
+            patientService.createPatient(patient);
+            
+            
+            Patient patient2 = new Patient();
+            patient.setActive(true);
+            patient.setBirthdate(new Date());
+            patient.setFirstname("James");
+            patient.setLastname("Brown");
+            patient.setGender("male");
+            patient.setHealthStatus("bad");
+            
+            patient.setMartialStatus("single");
+            patient.setPatientRegisterDate(new Date());
+            patient.setPhone("077777777");
+            patient.setStatus(1);
+            patientService.createPatient(patient2);
+        }
+        
 
 }
